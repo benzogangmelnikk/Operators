ff6214bd9e0975fd58dc351bd3a2cf612984b842
Cookie <eg.melnikk2@gmail.com>
2022-04-02 17:11:39 +0300

    Выполнено задание "wall"

--- 07_wall.py
+++ 07_wall.py
@@ -5,7 +5,18 @@ import simple_draw
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-
-# TODO здесь ваш код
+left_bottom = simple_draw.get_point(0, 550)
+right_top = simple_draw.get_point(100, 600)
+for row in range(12):
+    for column in range(12):
+        simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_DARK_ORANGE, width=2)
+        left_bottom.x = left_bottom.x + 100
+        right_top.x = right_top.x + 100
+    left_bottom.y = left_bottom.y - 50
+    right_top.y = right_top.y - 50
+    if row % 2 == 0:
+        left_bottom.x, right_top.x = -50, 50
+    else:
+        left_bottom.x, right_top.x = 0, 100
 
 simple_draw.pause()
